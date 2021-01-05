bc111d9e77325b8aa4d715dc671d5e7730e19b1f
Buni Emris <bsaydaliev@inbox.ru>
2021-01-05 20:02:32 +0500

    quiz test components

---
+++ src/Components/Quiz.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react'
+import {QuizData} from './QuizData'
+import '../styles.css'
+//import React, { useState, useEffect } from 'react';
+export class Quiz extends Component {
+
+    constructor(props) {
+        super(props)
+    
+        this.state = {
+            userAnswer:null,    
+            currentIndex:0, 
+            options: [],       
+            quizEnd: false, 
+            score: 0,     
+            disabled: true,
+        }
+    }
+
+    loadQuiz =()=>{
+        const {currentIndex} = this.state;
+        this.setState(()=>{
+            return {
+            question: QuizData[currentIndex].question,
+            options : QuizData[currentIndex].options,
+            answer: QuizData[currentIndex].answer 
+            }
+        })
+    }
+
+    nextQuestionHander = () => {
+    const {userAnswer, answer, score} = this.state
+    this.setState({
+        currentIndex: this.state.currentIndex + 1
+    })
+
+    
+    if(userAnswer === answer){
+        this.setState({
+            score: score + 1
+        })
+    }
+}
+
+componentDidMount(){
+    this.loadQuiz();
+}
+checkAnswer = answer => {
+    this.setState({
+        userAnswer: answer,
+        disabled:false
+    })
+}
+
+
+componentDidUpdate(prevProps, prevState){
+    const{currentIndex} = this.state;
+    if(this.state.currentIndex !== prevState.currentIndex){
+        this.setState(() => {
+            return {
+                disabled: true,
+                question: QuizData[currentIndex].question,
+                options : QuizData[currentIndex].options,
+                answer: QuizData[currentIndex].answer          
+            }
+        });
+
+    }
+}
+    finishHandler =() => {
+        const { userAnswer, answer, score } = this.state
+        if (userAnswer === answer) {
+        this.setState({
+        score: score + 1
+})
+}
+    if(this.state.currentIndex === QuizData.length -1){
+        this.setState({
+            quizEnd:true
+        })
+    }
+}
+    render() {
+        const {question, options, currentIndex, userAnswer, quizEnd} = this.state
+        if(quizEnd){
+            return(
+                <div>
+                    <h1> Final score is {this.state.score} points </h1>
+                                <h3>Thank you !!! </h3>
+                </div>
+            )
+        }
+        return (
+            <div>
+                <h2>{question}</h2>
+                <span>{`Question ${currentIndex +1} of ${QuizData.length}`}</span>
+                {
+                    options.map(option =>
+                        <p key= {option.id} className={`optionsStyle ${userAnswer === option? "selected":null}`}
+                        onClick={()=> this.checkAnswer(option)}>
+                            {option}
+                        </p>
+                        )
+                }
+                {currentIndex < QuizData.length - 1 && 
+                <button disabled ={this.state.disabled} onClick={this.nextQuestionHander} className={`nextButton`}>
+                    Next
+                </button>}
+                {currentIndex === QuizData.length-1 && <button onClick={this.finishHandler}
+                disabled={this.state.disabled} className={`finishButton`}>Finish</button>}
+
+            </div>
+        )
+    }
+}
+
+export default Quiz
---
+++ src/Components/QuizData.js
@@ -0,0 +1,47 @@
+export const QuizData = [
+    {
+        id: 0,
+        question: `What is the capital of Uzbekistan?`,
+        options: [`New Delhi`, `Tashkent`, `Nursultan`, `Osh`],
+        answer: `Tashkent`
+    },
+    {
+        id: 1,
+        question: `Who is the founder of Tesla company?`,
+        options: [`Bill Gates`, `Elon Musk`, `Jim Kerry`, `Bunyod`],
+        answer: `Elon Musk`
+    },
+    {
+        id: 2,
+        question: `What is the capital of Australia?`,
+        options: [`Mumbai`, `Dubai`, `Washington`, `Sydney`],
+        answer: `Sydney`
+    },
+    {
+        id: 3,
+        question: `How much is the ticket price for Rockofeller tower?`,
+        options: [`45$`, `30$`, `100$`, `free`],
+        answer: `45$`
+    },
+   
+    {
+        id: 4,
+        question: `Which car is more expensive?`,
+        options: [`Corrolla`, `Malibu`, `Sonata`, `Bugatti`],
+        answer: `Bugatti`
+    },
+    {
+        id: 5,
+        question: `Alberta is a province of which country?`,
+        options: [`USA`, `Canada`, `Austria`, `Italy`],
+        answer: `Canada`
+    },
+    {
+        id: 6,
+        question: `What is the largest country in the world?`,
+        options: [`UK`, `USA`, `China`, `Russia`],
+        answer: `Russia`
+    },
+      
+
+]
